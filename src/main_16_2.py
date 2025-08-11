a178ed44aa2096b3f93930485657ee7d1e0a2ec6
Роман Мстиславский <mstislavskijr@gmail.com>
2025-08-11 16:39:21 +0300

    add file main_16_2

---
+++ src/main_16_2.py
@@ -0,0 +1,194 @@
+from typing import List
+
+
+class Product:
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity) -> None:
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        """Метод возвращает строку содержимого продуктов в заданном формате"""
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        """Метод сложения который складывает товары только из одинаковых классов продуктов"""
+
+        if type(self) == type(other):
+            return (self.__price * self.quantity) + (other.__price * other.quantity)
+        raise TypeError("Нельзя складывать объекты разных классов")
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        # доп.задание при понижении цены (проверка цены)
+        try:
+            current_price = self.__price
+            if new_price < current_price:
+                confirmation = input(
+                    f"Цена понижается с {current_price} до {new_price}."
+                    f"Если хотите понизить цену введите 'y', либо вернуть текущую цену 'n': "
+                )
+                if confirmation.lower() != "y":
+                    print("Изменение цены отменено")
+                    return
+        except AttributeError:
+            pass
+
+        self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data: dict, existing_products: list):
+        """
+        Создает новый товар с проверкой на дубликаты
+        Параметры:
+        - product_data: словарь с данными товара
+        - existing_products: список существующих товаров для проверки
+        Возвращает: объект Product
+        """
+        if existing_products is None:
+            existing_products = []
+
+        name = product_data["name"]
+        description = product_data["description"]
+        price = float(product_data["price"])
+        quantity = int(product_data["quantity"])
+
+        for existing_product in existing_products:
+            if existing_product.name.lower() == name.lower():
+                existing_product.quantity = quantity  # Объединяем количество
+                existing_product.price = max(existing_product.price, price)  # Берём максимальную цену продукта
+                print(f"Товар {name} уже существует. Объединено количество и выбрана наибольшая цена")
+                return existing_product
+
+        return cls(name, description, price, quantity)  # Если дубликатов нет - создаем новый товар
+
+
+class Category:
+
+    name: str
+    description: str
+    products: List[Product]
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products) -> None:
+
+        self.name = name
+        self.description = description
+        self.__products = products if products else []
+        self.__products_count = len(products)  # Счетчик товаров конкретной категории
+
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
+
+    def __str__(self):
+        """Метод возвращает строку содержимого категории в заданном формате"""
+        summ_products = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {summ_products} шт."
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{self.name}, {product.price} руб. Остаток: {self.product_count} шт.\n"
+        return products_str
+
+    def add_product(self, product: Product) -> None:
+        """метод, который добавляет продукт в категорию,
+        таким образом, чтобы не было возможности добавить
+        вместо продукта или его наследников любой другой объект
+        """
+        if not isinstance(product, Product):  # ДОБАВЛЕНО: проверка на тип
+            raise TypeError("Можно добавить только объект класса Product или его наследника")
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+class Smartphone(Product):
+    """Новый класс наследник класса Product"""
+
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color) -> None:
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """Новый класс наследник класса Product"""
+
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color) -> None:
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
+    print(product1.price)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
+
+    category1 = Category("Смартфоны",
+                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         [product1, product2, product3])
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category2 = Category("Телевизоры",
+                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+                         [product4])
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
