509403698508f9cffd55a8d078e57c653e2729f7
Роман Мстиславский <mstislavskijr@gmail.com>
2025-08-15 20:17:54 +0300

    homwork

--- try_except_study.py
+++ try_except_study.py
@@ -53,16 +53,43 @@
 #     my_object.func3()
 
 
-class ShellScript:
+class ShellException(Exception): # Базовый класс для исключений
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Ошибка обработки скрипта"
+
+    def __str__(self):
+        return self.message
+
+
+class ShellEmptyException(ShellException):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Скрипт пустой"
+
+class ShellShebangException(ShellException):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Отсутствует шебанг"
+
+
+class ShellScript: # интерфейс, который передаем пользователю
     def __init__(self, content):
-        pass
+        if not content:
+            raise ShellEmptyException()
+        elif content[0:2] != "#!":
+            raise ShellShebangException()
+        self.eval()
 
     def eval(self):
         pass
 
+
 if __name__ == "__main__":
-    bash_content = ""
+    # bash_content = ""
+    bash_content = "!/bin/bash"
     try:
         shell_script = ShellScript(bash_content)
-    except:
-        pass
+    except ShellEmptyException as e:
+        print(e)
+        print("Передайте не пустой файл скрипта")
+    except ShellShebangException as e:
+        print(e)
+        print("Валидация на шебанг не пройдена")
