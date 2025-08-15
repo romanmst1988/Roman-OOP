be874aa4901c571ca57138839404173834d95990
Роман Мстиславский <mstislavskijr@gmail.com>
2025-08-15 20:47:46 +0300

    homwork

--- src/main_17_1.py
+++ src/main_17_1.py
@@ -50,6 +50,9 @@ class Product(BaseProduct, ReprMixin):
     """Класс продукта, наследующий от BaseProduct и ReprMixin"""
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+
         super().__init__(name=name, description=description, price=price, quantity=quantity)
         self.__price = price
 
@@ -154,23 +157,31 @@ class Category:
         self.__products.append(product)
         Category.product_count += 1
 
+    def middle_price(self) -> float:
+        """Метод для подсчета среднего ценника всех товаров в категории"""
+        try:
+            total = sum(product.price for product in self.__products)
+            return total / len(self.__products)
+        except ZeroDivisionError:
+            return 0.0
 
-# if __name__ == '__main__':
-#     try:
-#         product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
-#     except ValueError as e:
-#         print(
-#             "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
-#     else:
-#         print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
-#
-#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-#
-#     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
-#
-#     print(category1.middle_price())
-#
-#     category_empty = Category("Пустая категория", "Категория без продуктов", [])
-#     print(category_empty.middle_price())
+
+if __name__ == '__main__':
+    try:
+        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+    except ValueError as e:
+        print(
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
+    else:
+        print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
+
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
+
+    print(category1.middle_price())
+
+    category_empty = Category("Пустая категория", "Категория без продуктов", [])
+    print(category_empty.middle_price())
